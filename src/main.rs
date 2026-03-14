3d57c281d87554a30e943c495a142d16aa299442
BenAidlin <ben.aidlin@gmail.com>
2026-03-14 16:51:34 +0200

    feat: implement Gossipsub messaging with periodic heartbeats and manual peer dialing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use tracing::{info, info_span};
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use libp2p::futures::StreamExt;
-use libp2p::{swarm::SwarmEvent, mdns};
+use libp2p::{swarm::SwarmEvent, mdns, gossipsub};
+use std::time::Duration;
+use crate::p2p::behaviour::SOCIALKUBE_TASK_TOPIC;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -28,23 +30,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut swarm = p2p::host::build_swarm().await?;
     info!("Node initialized with PeerID: {}", swarm.local_peer_id());
 
+    // Subscribe to Gossipsub topic
+    let topic = gossipsub::IdentTopic::new(SOCIALKUBE_TASK_TOPIC);
+    swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
+    info!("Subscribed to topic: {}", SOCIALKUBE_TASK_TOPIC);
+
     // Listen on all interfaces with a random OS-assigned port
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
+    // Heartbeat ticker for testing
+    let mut tick = tokio::time::interval(Duration::from_secs(10));
+
     loop {
         tokio::select! {
+            _ = tick.tick() => {
+                let message = format!("Heartbeat from {}", swarm.local_peer_id());
+                if let Err(e) = swarm.behaviour_mut().gossipsub.publish(topic.clone(), message.as_bytes()) {
+                    tracing::error!("Publish error: {:?}", e);
+                }
+            }
             event = swarm.select_next_some() => match event {
                 SwarmEvent::NewListenAddr { address, .. } => {
                     info!("Listening on {:?}", address);
                 }
+                SwarmEvent::ConnectionEstablished { peer_id, endpoint, .. } => {
+                    info!("Successfully connected to {} at {:?}", peer_id, endpoint);
+                }
                 SwarmEvent::Behaviour(p2p::behaviour::SocialKubeEvent::Mdns(mdns::Event::Discovered(list))) => {
                     for (peer_id, multiaddr) in list {
                         info!("mDNS discovered: {} at {}", peer_id, multiaddr);
-                        swarm.behaviour_mut().kademlia.add_address(&peer_id, multiaddr);
+                        swarm.behaviour_mut().kademlia.add_address(&peer_id, multiaddr.clone());
+                        let _ = swarm.dial(multiaddr);
                     }
                 }
-                SwarmEvent::Behaviour(p2p::behaviour::SocialKubeEvent::Gossip(event)) => {
-                     info!("Gossipsub event: {:?}", event);
+                SwarmEvent::Behaviour(p2p::behaviour::SocialKubeEvent::Gossip(gossipsub::Event::Message { 
+                    propagation_source, 
+                    message, 
+                    .. 
+                })) => {
+                    let msg_str = String::from_utf8_lossy(&message.data);
+                    info!("Got message: '{}' from {:?}", msg_str, propagation_source);
                 }
                 // Handle other events as needed
                 _ => {}
